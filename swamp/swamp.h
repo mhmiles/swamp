f448e710131c029fe9df9c52464f753a382066e7
Miles Hollingsworth <mhmiles@me.com>
2017-11-30 19:00:30 -0800

    add carthage

---
+++ swamp/swamp.h
@@ -0,0 +1,18 @@
+//
+//  swamp.h
+//  swamp
+//
+//  Created by Miles Hollingsworth on 11/30/17.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swamp.
+FOUNDATION_EXPORT double swampVersionNumber;
+
+//! Project version string for swamp.
+FOUNDATION_EXPORT const unsigned char swampVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swamp/PublicHeader.h>
+
+
